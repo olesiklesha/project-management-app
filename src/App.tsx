f2171f659e854a6f900bd443a4efceb2b3055f5f
olesiklesha <olesik736@gmail.com>
2022-05-03 15:19:36 +0300

    feat: add fonts

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>The quick brown fox jumps over the lazy dog.</h1>
+      <h2>The quick brown fox jumps over the lazy dog.</h2>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
