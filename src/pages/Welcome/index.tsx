fcabfbfdb7c6d5c4ae5a85851d9c9d1100cd428c
Roman <sholomitskyR@yandex.by>
2022-05-09 00:44:16 +0300

    feat: add logo, remove styled component frome Welcome

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,52 +1,23 @@
 import React from 'react';
-import {
-  AppBar,
-  Button,
-  BottomNavigation,
-  Box,
-  Typography,
-  Container,
-  styled,
-  Grid,
-} from '@mui/material';
+import { AppBar, Button, BottomNavigation, Box, Typography, Container, Grid } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { AppRoutes, TEAM_INFO } from '../../constants';
 import { useTranslation } from 'react-i18next';
-import theme from "../../theme"
 import YoutubeIframe from '../../components/YoutubeIframe';
 import Profile from '../../components/Profile';
+import HomeIcon from '../../components/Header/home-icon.styled';
+import LinkRss from '../../components/Footer/LinkRss.styled';
+import { CustomizedBox, CustomizedImg, CustomizedTypography } from './WelcomeComponent.styled';
 
 function Welcome() {
   const { t } = useTranslation();
-
-  const CustomizedBox = styled(Box)`
-    background: linear-gradient(0deg, #fff, ${theme.palette.background.paper} 100%);
-    min-height: 80vh;
-  `;
-
-  const CustomizedTypography = styled(Typography)`
-    font-family: Ubuntu;
-    font-size: 3rem;
-    text-align: center;
-    margin-bottom: 3rem;
-    font-weight: bold;
-  `;
-
-  const CustomizedImg = styled('img')`
-    min-height: calc(80vh - 64px);
-  `;
 
   return (
     <>
       <AppBar color="primary" position="relative" sx={{ boxShadow: 'none' }}>
         <BottomNavigation component="nav" sx={{ padding: '16px', justifyContent: 'space-between' }}>
           <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style={{ width: 40 }}>
-              <path
-                d="M0 96C0 60.65 28.65 32 64 32H448C483.3 32 512 60.65 512 96V416C512 451.3 483.3 480 448 480H64C28.65 480 0 451.3 0 416V96zM64 416H224V160H64V416zM448 160H288V416H448V160z"
-                fill="#ffd803"
-              />
-            </svg>
+            <HomeIcon fontSize="large" />
             <Typography
               variant="h4"
               gutterBottom
@@ -58,10 +29,10 @@ function Welcome() {
           </Box>
           <Box>
             <Button component={Link} to={AppRoutes.LOG_IN} color="secondary">
-            {t('pages.welcomePage.logInButton')}
+              {t('pages.welcomePage.logInButton')}
             </Button>
             <Button component={Link} to={AppRoutes.SIGN_IN} color="secondary" variant="contained">
-            {t('pages.welcomePage.signInButton')}
+              {t('pages.welcomePage.signInButton')}
             </Button>
           </Box>
         </BottomNavigation>
@@ -104,7 +75,15 @@ function Welcome() {
         <CustomizedTypography>How to use app</CustomizedTypography>
         <YoutubeIframe />
       </Container>
-      <Container sx={{ display: 'flex', justifyContent: 'center' }} component="section">
+      <Container
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+        component="section"
+      >
         <Grid item xs={12} sm={8} md={6}>
           <CustomizedTypography>Team</CustomizedTypography>
           {TEAM_INFO.map((el, index) => (
@@ -117,6 +96,10 @@ function Welcome() {
             />
           ))}
         </Grid>
+        <LinkRss sx={{ fontSize: 200 }} />
+        <Typography sx={{ fontFamily: 'Roboto', fontSize: '1rem', fontWeight: 'normal' }}>
+          © 2022
+        </Typography>
       </Container>
     </>
   );
