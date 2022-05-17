925d9475bae42c13daf36ce77b77f5c81226e6f7
olesiklesha <olesik736@gmail.com>
2022-05-17 19:29:11 +0300

    feat: create the utils function getNextOrder, change tags in service

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 import { AUTH, TOKEN_LIFETIME } from '../constants';
-import { IRequestError, IUserData } from '../models/apiModels';
+import { IColumnData, IRequestError, ITask, IUserData } from '../models';
 
 export function getLocalAuth() {
   const localStorageAuth = window.localStorage.getItem(AUTH);
@@ -30,4 +30,10 @@ export function getCurrentUser(data: IUserData[]) {
 
 export function logOut() {
   window.localStorage.removeItem(AUTH);
+}
+
+export function getNextOrder(data: IColumnData[] | ITask[]) {
+  const orders = data.map((el) => el.order);
+  orders.sort((a, b) => b - a);
+  return orders[0] + 1;
 }
