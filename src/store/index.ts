171deab4ed5f548cd7f00bb810717a6d04bfb831
ArtyomKr <tyomik.krasnitsky@gmail.com>
2022-05-17 12:51:44 +0300

    feat: add middleware and slice for global error

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,33 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import boardsSlice from './reducers/boardsSlice';
 import authSlice from './reducers/auth';
+import globalErrorSlice from './reducers/globalErrorSlice';
+import { globalErrorSlice as globalErrorState } from './reducers/globalErrorSlice';
 import { appApi } from '../services';
+
+import { MiddlewareAPI, isRejectedWithValue, Middleware } from '@reduxjs/toolkit';
+
+export const rtkQueryErrorLogger: Middleware = (api: MiddlewareAPI) => (next) => (action) => {
+  if (isRejectedWithValue(action)) {
+    console.warn(action.payload);
+    api.dispatch(globalErrorState.actions.updateError(action.payload));
+  }
+
+  return next(action);
+};
 
 const rootReducer = combineReducers({
   authSlice,
   boardsSlice,
+  globalErrorSlice,
   [appApi.reducerPath]: appApi.reducer,
 });
 
 export const setupStore = () => {
   return configureStore({
     reducer: rootReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(appApi.middleware),
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(appApi.middleware).concat(rtkQueryErrorLogger),
   });
 };
 
