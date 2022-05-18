04c8d395480960d25e84d3a61f65bf17e28e3faa
ArtyomKr <ArtyomKr@users.noreply.github.com>
2022-05-18 18:16:30 +0300

    refactor: change rendered data to hook

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Alert, Box, Button, CircularProgress, Container, Grid, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
-import { useAppSelector } from '../../hooks/redux';
 import { BoardCreator, Modal, ShortBoard } from '../../components';
 import { useGetAllBoardsQuery } from '../../services';
 import { apiErrorParser } from '../../utils';
@@ -10,9 +9,9 @@ import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
 
 function Main() {
   const { t } = useTranslation();
-  const { boards } = useAppSelector((state) => state.boardsSlice);
-  const { isLoading, isError, error } = useGetAllBoardsQuery();
+  const { data, isLoading, isError, error } = useGetAllBoardsQuery();
   const [isOpened, setOpened] = useState(false);
+  const boards = data || [];
 
   const toggleIsOpened = () => {
     setOpened((prev) => !prev);
@@ -43,14 +42,16 @@ function Main() {
           />
         )}
         {isError && <Alert severity="error">{apiErrorParser(error as IRequestError, t)}</Alert>}
-        <Grid container spacing={2} sx={{ mt: 2, mb: 4 }}>
-          {boards.length > 0 &&
-            boards.map((board) => (
-              <Grid item xs={12} sm={6} md={3} key={board.id}>
-                <ShortBoard id={board.id} title={board.title} />
-              </Grid>
-            ))}
-        </Grid>
+        {!isLoading && (
+          <Grid container spacing={2} sx={{ mt: 2, mb: 4 }}>
+            {boards.length > 0 &&
+              boards.map((board) => (
+                <Grid item xs={12} sm={6} md={3} key={board.id}>
+                  <ShortBoard id={board.id} title={board.title} />
+                </Grid>
+              ))}
+          </Grid>
+        )}
       </Container>
       <Modal isOpened={isOpened} onCancel={toggleIsOpened}>
         <BoardCreator onCancel={toggleIsOpened} />
