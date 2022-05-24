7f4cda2471bb92c02e26ff739bd56f616bdfafef
ArtyomKr <ArtyomKr@users.noreply.github.com>
2022-05-24 22:02:49 +0300

    refactor: delete old icons

--- src/components/AppIcon/index.tsx
+++ src/components/AppIcon/index.tsx
@@ -1,5 +1,5 @@
 import SvgIcon, { SvgIconProps } from '@mui/material/SvgIcon';
-import { ReactComponent as Logo } from './ice-cream11.svg';
+import { ReactComponent as Logo } from './ice-cream.svg';
 
 function AppIcon(props: SvgIconProps) {
   return (
