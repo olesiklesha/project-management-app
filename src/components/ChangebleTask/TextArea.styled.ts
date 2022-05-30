fd29a0339d0a1e1c6882350dd8bb49e5ad803b65
Roman <sholomitskyR@yandex.by>
2022-05-30 11:07:09 +0300

    feat: add styled TextArea

---
+++ src/components/ChangebleTask/TextArea.styled.ts
@@ -0,0 +1,10 @@
+import { styled, TextareaAutosize } from '@mui/material';
+
+export const CustomizedTextareaAutosize = styled(TextareaAutosize)`
+  width: 256px;
+  font-size: 1rem;
+  font-family: Roboto;
+  line-height: 1.4;
+  padding: 10px;
+  resize: vertical;
+`;
---
+++ src/components/TaskModal/TextArea.styled.ts
@@ -0,0 +1,12 @@
+import { styled, TextareaAutosize } from '@mui/material';
+
+export const CustomizedTextareaAutosize = styled(TextareaAutosize)`
+  width: 90%;
+  font-size: 1rem;
+  font-family: Roboto;
+  line-height: 1.5;
+  padding: 10px;
+  resize: none;
+  margin-left: 3rem;
+  margin-bottom: 0.5rem;
+`;
